3cd147445e8f55d962c96c9b30275fd17593f209
joaoppessoa <joaoppessoa25@gmail.com>
2021-10-04 15:31:10 -0300

    Vinculando pessoas as receitas.

--- receitasjp/models.py
+++ receitasjp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime  import datetime
+from pessoas.models import Pessoas
 
 class Receita(models.Model):
     nome_receita = models.CharField(max_length=200)
@@ -8,4 +9,5 @@ class Receita(models.Model):
     tempo_preparo = models.IntegerField()
     redimento = models.TextField(max_length=100)
     categoria = models.CharField(max_length=100)
+    pessoa = models.ForeignKey(Pessoas, on_delete=models.CASCADE)
     data_receita = models.DateTimeField(default=datetime.now, blank=True)
