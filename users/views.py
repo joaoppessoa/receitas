f65f73ce1fda19b9254d00303af6e45110b4a3a8
joaoppessoa <joaoppessoa25@gmail.com>
2021-10-13 23:25:22 -0300

    Utilizando formularios e requisçao POST para salvar usuarios.

--- users/views.py
+++ users/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 def cadastro(request):
-    return render(request, 'users/cadastro.html')
+    if request.method == 'POST':
+        post = request.POST
+
+        if not post['nome'].strip():
+            print("Campo Nome está em branco.")
+            return redirect('cadastro')
+        
+        if not post['email'].strip():
+            print("Campo E-mail está em branco.")
+            return redirect('cadastro')
+
+        if post['password'] != post['password2']:
+            print("As senhas não são iguais")
+            return redirect('cadastro')
+        
+        if User.objects.filter(email=post['email']).exists() or User.objects.filter(username=post['nome']).exists():
+            print("Este e-mail já está em uso.")
+            return redirect('cadastro')
+        
+        user = User.objects.create_user(
+            username=post['nome'], 
+            email=post['email'], 
+            password=post['password'])
+        user.save()
+
+        return redirect('login')
+    else:
+        return render(request, 'users/cadastro.html')
+
+    #return render(request, 'users/cadastro.html')
 
 def login(request):
     return render(request, 'users/login.html')
